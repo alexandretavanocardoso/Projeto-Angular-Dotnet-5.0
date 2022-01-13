2a0766b7b20bfd04dcc9f153574bbe571d05449d
Alexandre Tavano <AlexandreTavanoDeveloper@outlook.com>
2022-01-12 21:25:46 -0300

    Criando Context e ligação com o SQLite

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GET")]
+        public IEnumerable<Evento> Get()
+        {
+           return _context.Eventos;
+        }
+    }
+}
